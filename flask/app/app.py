81b28cbfab8d70c1f6df5d9640fa9b13336f90c7
dogperrywu <dogperrywu10@yahoo.com.tw>
2022-04-06 10:24:03 +0800

    改置頂

--- flask/app/app.py
+++ flask/app/app.py
@@ -97,5 +97,4 @@ def report():
                             bi_df = bi_df.head(5))
     
 if __name__ == '__main__':
-     app.run(host='127.0.0.1', port=8000)
-     app.run(debug=True)
+     app.run(host='127.0.0.1', port=8000,debug=True)
