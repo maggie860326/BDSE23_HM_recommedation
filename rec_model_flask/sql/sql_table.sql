5c4358a26162f0a05f5d5f635562ac51ab152bcb
maggie860326 <maggie860326@gmail.com>
2022-04-12 13:26:18 +0800

    2022/04/12 刪除多餘檔案

--- rec_model_flask/sql/sql_table.sql
+++
@@ -1,9 +0,0 @@
-# create table by sql query
-create TABLE HM.cust_art_list(
-    customer_id varchar(255),
-    article_id varchar(255));
-
-insert into HM.cust_art_list(customer_id,article_id)
-	select customer_id,
-       article_id 
-from HM.transactions_train;
